18020921363dded8851e4154a25a3f1cd5a2ce0c
Konstantin-Scheffold <82522803+Konstantin-Scheffold@users.noreply.github.com>
2021-05-08 11:28:00 +0200

    Initial commit

---
+++ Get_Mesh.py
@@ -0,0 +1,101 @@
+import trimesh
+from Def_functions import make_a_mesh
+from Read_dicom import Data_PD
+from Read_dicom import Data_CT
+import scipy
+
+
+#make_a_mesh(Data_PD, 'PD_final.ply')
+Data_rot_CT = Data_CT
+Data_rot_CT.data = scipy.ndimage.rotate(Data_CT.data, 45, axes = (1, 0), reshape = True)
+make_a_mesh(Data_rot_CT, 'CT_rotated.ply')
+
+
+# verts, faces, normals, values = measure.marching_cubes(Data_not_rot_PD.data, gradient_direction='ascent', spacing = Data_not_rot_PD.spacing)
+#
+# trimesh.util.attach_to_log()
+#
+# mesh = trimesh.Trimesh(vertices= verts,
+#                        faces= faces,
+#                        process=False)
+#
+# trimesh.exchange.export.export_mesh(mesh = mesh, file_obj = 'PD_3.ply')
+
+
+#
+# # some formats represent multiple meshes with multiple instances
+# # the loader tries to return the datatype which makes the most sense
+# # which will for scene-like files will return a `trimesh.Scene` object.
+# # if you *always* want a straight `trimesh.Trimesh` you can ask the
+# # loader to "force" the result into a mesh through concatenation
+# ####mesh = trimesh.load('CesiumMilkTruck.glb', force='mesh')
+#
+# # mesh objects can be loaded from a file name or from a buffer
+# # you can pass any of the kwargs for the `Trimesh` constructor
+# # to `trimesh.load`, including `process=False` if you would like
+# # to preserve the original loaded data without merging vertices
+# # STL files will be a soup of disconnected triangles without
+# # merging vertices however and will not register as watertight
+# #mesh = trimesh.load('../models/featuretype.STL')
+#
+# # is the current mesh watertight?
+# mesh.is_watertight
+#
+# # what's the euler number for the mesh?
+# mesh.euler_number
+#
+# # the convex hull is another Trimesh object that is available as a property
+# # lets compare the volume of our mesh with the volume of its convex hull
+# print(mesh.volume / mesh.convex_hull.volume)
+#
+# # since the mesh is watertight, it means there is a
+# # volumetric center of mass which we can set as the origin for our mesh
+# mesh.vertices -= mesh.center_mass
+#
+# # what's the moment of inertia for the mesh?
+# mesh.moment_inertia
+#
+# # if there are multiple bodies in the mesh we can split the mesh by
+# # connected components of face adjacency
+# # since this example mesh is a single watertight body we get a list of one mesh
+# mesh.split()
+#
+# # facets are groups of coplanar adjacent faces
+# # set each facet to a random color
+# # colors are 8 bit RGBA by default (n, 4) np.uint8
+# for facet in mesh.facets:
+#     mesh.visual.face_colors[facet] = trimesh.visual.random_color()
+#
+# # preview mesh in an opengl window if you installed pyglet with pip
+# #mesh.show()
+#
+# # transform method can be passed a (4, 4) matrix and will cleanly apply the transform
+# mesh.apply_transform(trimesh.transformations.random_rotation_matrix())
+#
+# # axis aligned bounding box is available
+# mesh.bounding_box.extents
+#
+# # a minimum volume oriented bounding box also available
+# # primitives are subclasses of Trimesh objects which automatically generate
+# # faces and vertices from data stored in the 'primitive' attribute
+# mesh.bounding_box_oriented.primitive.extents
+# mesh.bounding_box_oriented.primitive.transform
+#
+# # show the mesh appended with its oriented bounding box
+# # the bounding box is a trimesh.primitives.Box object, which subclasses
+# # Trimesh and lazily evaluates to fill in vertices and faces when requested
+# # (press w in viewer to see triangles)
+# #(mesh + mesh.bounding_box_oriented).show()
+#
+#
+# trimesh.exchange.export.export_mesh(mesh = mesh, file_obj = 'PD_2.ply')
+#
+#
+#
+# #bounding spheres and bounding cylinders of meshes are also
+# # available, and will be the minimum volume version of each
+# # except in certain degenerate cases, where they will be no worse
+# # than a least squares fit version of the primitive.
+# print(mesh.bounding_box_oriented.volume,
+#       mesh.bounding_cylinder.volume,
+#       mesh.bounding_sphere.volume)
---
+++ Read_dicom.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pydicom
+import os
+
+# Store volume data together with grid size and position in space
+
+
+class VolumeData:
+    """
+    Storage class for a Volume inside a coordinate system. The data
+    itself is stored as a np.ndarray. Additional to this the spacing
+    and the position is stored.
+    """
+
+    def __init__(self,
+                 data,
+                 spacing,
+                 position=np.zeros(3)):
+        """
+        Initializes the VolumeData class
+        Args:
+            data: Volume data
+            spacing: Voxel size
+            position: Position in an arbitrary coordinate system
+        """
+        # Store volume data
+        assert len(data.shape) == 3
+        self.data = np.asarray(data, dtype=float)
+        # Store size of the cells
+        assert len(spacing) == 3
+        self.spacing = np.asarray(spacing, dtype=float)
+        # Store position in 3D space
+        assert len(position) == 3
+        self.position = np.asarray(position, dtype=int)
+
+
+def read_dicom_stack(filepath):
+
+    """
+    Reads a stack of DICOM images into an AnnotatedImage
+    To read in the stack all filenames with the .dcm ending are stored
+    in an array then sorted and read in one after another.
+    To create AnnotatedImage.data all slices are appended to one lager
+    three dimensional ndarray.
+    The position is calculated from the first and last slice. It is
+    defined as the center of both slices.
+    Spacing is calculated from the first two slices. x- and y-direction
+    can be taken directly but z-direction needs to be calculated using
+    the difference between the position of those two slices.
+    The data is stored as [z, y, x]
+    Args:
+        filepath: Path to the image folder.
+    Returns: AnnotatedImage with data, (voxel) size and position
+    """
+
+    #  from: https://pyscience.wordpress.com/2014/09/08/dicom-in-python-
+    #  importing-medical-image-data-into-numpy-with-pydicom-and-vtk/
+    file_list = []
+    #  Read in filenames from directory and sort them
+    for root, dirs, files in os.walk(filepath):
+        for filename in files:
+            if ".dcm" in filename.lower():
+                file_list.append(os.path.join(root, filename))
+    file_list.sort()
+    #  Read in first reference file to define shape of data
+    ref_data = pydicom.read_file(file_list[0])
+    data_shape = (len(file_list), int(ref_data.Rows), int(ref_data.Columns))
+    data = np.zeros(data_shape, int)
+    #  Read in image stack and store in single ndarray
+    for filename in file_list:
+        data_slice = pydicom.dcmread(filename)
+        data[file_list.index(filename), :, :] = data_slice.pixel_array
+    # Read and calculate voxel size
+    # The voxel size within one slice in given. Between the slices
+    # (z-direction) has to be calculated as the difference in the
+    # position of two slices.
+    ref_data_second = pydicom.read_file(file_list[1])
+    spacing3 = (ref_data_second.ImagePositionPatient[2]
+                - ref_data.ImagePositionPatient[2])
+    spacing = np.asarray([float(spacing3),
+                          float(ref_data.PixelSpacing[1]),
+                          float(ref_data.PixelSpacing[0])])
+    # Calculate the volume position
+    # The stored position in the dicom file points to top center of the
+    # lower left last corner of the slice. Center in x,y direction and
+    # on end of the z direction. (0.5,0.5,1) for a unit cell
+    position = np.asarray([float(ref_data.ImagePositionPatient[2]),
+                           float(ref_data.ImagePositionPatient[0]),
+                           float(ref_data.ImagePositionPatient[1])])
+    position = position - (np.asarray([1, 0.5, 0.5]) * spacing)
+    # To get the real lover left last corner the stated position has to
+    # be decreased by half the spacing in x and y direction and a full
+    # spacing in z direction
+
+    #  Delete ref_data*
+    del ref_data
+    del ref_data_second
+    return VolumeData(data, spacing, position)
+
+# Daten einlesen
+
+
+Data_CT = read_dicom_stack('Data_BA_Thesis/NewExportsNov2020/CT')
+Data_PD = read_dicom_stack('Data_BA_Thesis/NewExportsNov2020/PD')
