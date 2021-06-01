716a7fbb2bb4598d05ed68b299a318ee585989c8
Konstantin-Scheffold <82522803+Konstantin-Scheffold@users.noreply.github.com>
2021-06-01 11:59:55 +0200

    making subsamples of PD_Data works

--- Def_functions.py
+++ Def_functions.py
@@ -1,7 +1,7 @@
 import trimesh
 from skimage import measure
 import numpy as np
-from Read_dicom import Data_PD
+
 
 def make_a_mesh(vol, name, iso):
     cm_verts, cm_faces, cm_normals, _ = measure.marching_cubes(
@@ -13,25 +13,26 @@ def make_subsamples_3d(vol, size):
     subsamples_data = []
     # axis(Subsampled_data):[x,y,z]
     subsamples_splitting = np.array_split(vol, size, axis = 0)
-    # axis(Subsampled_data):[sub_x, x, y, z]
 
+    # axis(Subsampled_data):[sub_x, x, y, z]
     for i in np.arange(size):
         subsamples_splitting[i] = np.array_split(subsamples_splitting[i], size, axis=1)
-    # axis(Subsampled_data):[sub_x, sub_y, sub_z x, y, z]
 
+    # axis(Subsampled_data):[sub_x, sub_y, sub_z x, y, z]
     for i in np.arange(size):
         for j in np.arange(size):
             subsamples_splitting[i][j] = np.array_split(subsamples_splitting[i][j], size, axis=2)
-    # axis(Subsampled_data):[sub_x, sub_y, sub_z x, y, z]
-    print(np.shape(subsamples_splitting))
+
+# axis(Subsampled_data):[sub_x, sub_y, sub_z x, y, z]
     for i in np.arange(size):
         for j in np.arange(size):
             for k in np.arange(size):
                 subsamples_data.append(subsamples_splitting[i][j][k])
 
+    subsamples_data = np.asarray(subsamples_data, dtype=object)
+
     return subsamples_data
 
-
-# print(np.shape(Data_PD.data))
-# z = make_subsamples_3d(Data_PD.data, 2)
-# print(np.shape(z))
+# a = np.arange(364*308*308).reshape((364 , 308, 308))
+# a = make_subsamples_3d(a, 2)
+# print(np.shape(a))
--- sub_sampling.py
+++ sub_sampling.py
@@ -3,10 +3,18 @@ from Def_functions import make_subsamples_3d
 from Def_functions import make_a_mesh
 from Read_dicom import Data_PD
 from Read_dicom import Data_CT
+from skimage.transform import downscale_local_mean
 
-size = 2
-print_mesh = Data_PD
+size = 4
 
-for i in np.arange(size**3):
-    print_mesh.data = make_subsamples_3d(Data_PD.data[i], size)
-    make_a_mesh(print_mesh, 'Data_PD_subsample_{}'.format(i), 6000)
+# make size dividable by 4
+Data_PD.data = Data_PD.data[:-3,:-1,:-1]
+
+print(np.shape(Data_PD.data))
+#make_a_mesh(Data_PD, 'PD_Data_resized.ply', 6000)
+z = make_subsamples_3d(Data_PD.data, size)
+print(np.shape(z))
+
+for i in range(size**3-4):
+    Data_PD.data = z[i]
+    make_a_mesh(Data_PD, 'PD_Data_subsample{}.ply'.format(i), 6000)
